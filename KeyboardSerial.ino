5c8c910f778904a201fde104c610110481c64a60
SachinKonan <sachinkonan480@gmail.com>
2017-10-04 23:56:05 -0700

    added arduino

--- KeyboardSerial.ino
+++ KeyboardSerial.ino
@@ -2,13 +2,14 @@
 
 void setup() {
   // open the serial port:
+  delay(2000);
   Keyboard.press(KEY_LEFT_CTRL);
   delay(1000);
   Keyboard.press(KEY_ESC);
   delay(100);
   Keyboard.releaseAll();
   Keyboard.println("notepad");
-  delay(1000);
+  delay(4000);
   Keyboard.press(KEY_RETURN);
   delay(500);
   Keyboard.releaseAll();
@@ -56,37 +57,52 @@ void regularPrint(String x, int indent)
 void nw()
 {
   String lines[] = {
-    "--------------",
-    "|Sachin Konan|",
-    "--------------",
-    "Email: sachinkonan480@gmail.com",
-    "Phone: 480-621-9929",
-    "github: https://github.com/SachinKonan/",
-    "Education_",
-    "1. Hamilton High School?",
-    "Senior Courses:!",
-    "AP Government, AP Statistics, Honors Science Research, Engineering 102, Digital Photography, AP Economics, AP English Literature*",
-    "GPA: 4.0(Unweighted)!",
-    "+",
-    "Experience_",
-    "1.ASU Connection One Laboratory?",
-    "RF Design Student!",
-    "Worked with High Frequency Oscilloscopes and Spectrum Analyzers for Radar Project*",
-    "2. F1 in Schools?",
-    "Team Manager and CAD Designer!",
-    "Designed a car using Autodesk Inventor and ANSYS CFD and coordinated meetings and national flights*",
-    "3. Si Se Puede FRC Robotics?",
-    "Lead Programmer!",
-    "Wrote Extensive Java Code for general robot movement and control over robotic subsystems*",
-    "Wrote extensive Python Code that uses OpenCV and Keras for object detection and 3D Localization*",
-  };
+    F("--------------"),
+    F("|Sachin Konan|"),
+    F("--------------"),
+    F("Email: sachinkonan480@gmail.com"),
+    F("Phone: 480-621-9929"),
+    F("github: https://github.com/SachinKonan/"),
+    F("Education_"),
+    F("1. Hamilton High School?"),
+    F("Senior Courses:!"),
+    F("Maintained 4.0 Unweighted GPA and 12 AP Classes Total*"),
+    F("+"),
+    F("Experience_"),
+    F("1.ASU Connection One Laboratory?"),
+    F("Worked with High Frequency Oscilloscopes and Spectrum Analyzers for Radar Project!"),
+    F("+"),
+    F("Activities_"),
+    F("2. F1 in Schools?"),
+    F("Team Manager and CAD Designer!"),
+    F("Designed a car using Autodesk Inventor and ANSYS CFD and coordinated meetings and national flights*"),
+    F("3. Si Se Puede FRC Robotics?"),
+    F("Wrote Extensive Java Code for general robot movement and control over robotic subsystems!"),
+    F("4. Science Fair?"),
+    F("Developed a low-cost Doppler radar system capable of detecting buried earthquake victims (2015-2017)!"),
+    F("5. Science Bowl?"),
+    F("Founder and President -- mentored and formed a team with teacher Mrs.Bhagdev!"),
+    F("+"),
+    F("Awards_"),
+    F("1.2x International Science Fair Qualifier(16-17) & 2nd in Embed Systems & AirForce Award(17)?"),
+    F("2.2x 1st in Embed Systems at Arizona State Fair(17)?"),
+    F("3.F1 in Schools 2nd and 1st at Regional and National Qual(15-16)?"),
+    F("Best Engineered Car, Fastest Car, Social Media Special Awards?"),
+    F("4.Top 64 International Policy Debate?"),
+    F("5.2nd in Great AZ Code Hackathon(17)?"),
+    F("6.2nd and 1st at Phoenix Half Marathon(Age<17) (14-15)?"),
+    F("+"),
+    F("Volunteer_"),
+    F("1.Senior Center and 70+hrs(16)?"),
+    F("2.Frye Elementary and Red Dragon FLL Mentoring(15-17)?"),
+    };
   for (int x = 0; x < sizeof(lines); x++)
   {
     String current = lines[x];
     // key ? = regular, ! = indent one, * indent twice
     if (current.endsWith("_"))
     {
-      newSection(current, 150);
+      newSection(current, 137);
     }
     else if (current.endsWith("?"))
     {
@@ -106,14 +122,15 @@ void nw()
     }
     else
     {
-      padPrint(current, 150);
+      padPrint(current, 137);
     }
   };
+  Keyboard.release(KEY_RETURN);
 }
 
 
 void loop()
 {
-
+  Keyboard.release(KEY_RETURN);
 }
 
